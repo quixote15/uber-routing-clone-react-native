7e5c50e2ae8204fc08cb4d3c487ded8db75e6ef6
Tiago <tiago.santos@ayoapp.com.br>
2019-10-25 17:26:17 -0300

    Adiciona rotas

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable prettier/prettier */
 /* eslint-disable semi */
 /**
@@ -17,6 +18,8 @@ import {
   Text,
   Dimensions,
   StatusBar,
+  TextInput,
+  TouchableOpacity,
 } from 'react-native';
 
 import {
@@ -28,119 +31,182 @@ import {
 } from 'react-native/Libraries/NewAppScreen';
 
 import MapView from 'react-native-maps';
-
+import MapViewDirections from 'react-native-maps-directions';
+import getDirections from 'react-native-google-maps-directions';
+import {PermissionsAndroid} from 'react-native';
+import Geocoder from 'react-native-geocoding';
+const MAPS_API_KEY = 'AIzaSyA6sJPwAhHJGBEi9DjcLwmpIwsh1_hCFxk';
 const {height, width} = Dimensions.get('window');
 
 const App: () => React$Node = () => {
-  // const [latitude, setLatitude] = useState(-10.9141);
-  //  const [longitude, setLongitude] = useState(-37.6693);
+  const [latitude, setLatitude] = useState(-10.9141);
+  const [longitude, setLongitude] = useState(-37.6693);
   const [mapView, setMapView] = useState(null);
-  const [places, setPlaces] = useState([
-    {
-      id: 1,
-      title: 'Casa do café',
-      description: 'Café quentinho...',
-      latitude: -27.210671,
-      longitude: -49.63627,
-    },
-    {
-      id: 2,
-      title: 'RocketSeat',
-      description: 'Programação, empreendedorismo e mindset',
-      latitude: -27.200671,
-      longitude: -49.63627,
-    },
-    {
-      id: 3,
-      title: 'Casa do José',
-      description: 'José, tá em casa?',
-      latitude: -27.200671,
-      longitude: -49.62627,
-    },
-    ,
-  ]);
+  const [origin, setOrigin] = useState({ latitude: 42.3616132, longitude: -71.0672576 })
+  const [destination, setDestination] = useState({ latitude: 42.3730591, longitude: -71.033754 })
+  const [originText, setOriginText] = useState('');
+  const [destinationText, setDestinationText] = useState('');
 
-  useEffect(() => {
-    /*setTimeout(() => {
-      if (mapView) {
-        console.log(mapView.getCamera());
-        mapView.animateToCoordinate(
-          {
-            latitude: -10.6141,
-            longitude: -37.2693,
-          },
-          2000,
-        );
-      }
-    }, 3000);*/
-  }, [mapView]);
+  const handleButton = () => {
+    if (originText !== ''){
+      Geocoder.init(MAPS_API_KEY);
 
-  const _mapReady = () => {
-    places[0].mark.showCallout();
+      Geocoder.from(originText)
+        .then(json => {
+          const location = json.results[0].geometry.location;
+          console.log(location);
+          setOrigin({latitude: location.lat, longitude: location.lng});
+        })
+        .catch(error => console.warn(error));
+    } else {
+      alert('Digite a origem');
+    }
+
+    if (destinationText != '') {
+
+      Geocoder.init(MAPS_API_KEY); // use a valid API key
+
+      Geocoder.from(destinationText)
+      .then(json => {
+          var location = json.results[0].geometry.location;
+          console.log(location);
+          setDestination( { latitude: location.lat, longitude: location.lng } );
+
+      })
+      .catch(error => console.warn(error));
   }
 
-  const {latitude, longitude} = places[0];
+  else {
+      alert('Digite o destino ! ')
+  }
+
+  }
+
+
+  const requesLocationPermission = async() => {
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+        {
+          'title': 'Permissão de localização do APP',
+          'message': 'O aplicativo Airbnb clone precisa acessar seu mapa para que voce possa navegar.',
+        }
+      );
+
+      if (granted === PermissionsAndroid.RESULTS.GRANTED){
+        console.log('voce pode usar a navegacao');
+        return true;
+      } else {
+        console.log('permissao negada.');
+        return false;
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const getLocation = () => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      let newOrigin = {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+      };
+
+      setOrigin(newOrigin);
+
+    }, (err) => {
+      console.log('errpr');
+      console.log(err);
+
+    }, {enableHighAccuracy: true, timeout: 2000, maximumAge: 1000});
+  }
+
+  const handleGetGoogleMapDirections = () => {
+    const data = {
+      source: origin,
+      destination,
+      params: [
+        {
+          key: 'travelmode',
+          value: 'driving',
+        },
+      ],
+    };
+
+    getDirections();
+  }
+
+  useEffect(async ()=>{
+    let isGranted = false;
+    const checkPermission = async () => {
+     isGranted = await requesLocationPermission();
+     if (isGranted) {
+       getLocation();
+      }
+
+      getLocation();
+    };
+    checkPermission();
+  },[]);
+
+
   return (
     <View style={styles.container}>
       <MapView
         ref={map => setMapView(map)}
-        initialRegion={{
-          latitude,
-          longitude,
-          latitudeDelta: 0.0142,
-          longitudeDelta: 0.0231,
+        region={{
+          latitude: (origin.latitude + destination.latitude) / 2,
+          longitude: (origin.longitude + destination.longitude) / 2,
+          latitudeDelta: Math.abs(origin.latitude - destination.latitude) + Math.abs(origin.latitude - destination.latitude) * 0.1,
+          longitudeDelta: Math.abs(origin.longitude - destination.longitude)  + Math.abs(origin.longitude - destination.longitude) * 0.1,
+
         }}
         style={styles.MapView}
-        rotateEnabled={false}
-        scrollEnabled={false}
-        zoomEnabled={false}
-        showsPointsOfInterest={false}
-        showsBuildings={false}
-        onMapReady={_mapReady}>
-        {places.map(place => (
-          <MapView.Marker
-            ref={mark => {place.mark = mark}}
-            key={place.id}
-            title={place.title}
-            description={place.description}
-            coordinate={{
-              latitude: place.latitude,
-              longitude: place.longitude,
-            }}
-          />
-        ))}
-      </MapView>
+        loadingEnabled={true}
+        toolbarEnabled={true}
+        zoomControlEnabled={true}
+         >
 
-      <ScrollView
-        style={styles.placesContainer}
-        horizontal
-        showsHorizontalScrollIndicator={false}
-        pagingEnabled
-        onMomentumScrollEnd={e => {
-          const scrolled = e.nativeEvent.contentOffset.x;
+           <MapView.Marker coordinate={destination}>
+            <MapView.Callout onPress={handleGetGoogleMapDirections}>
+              <Text>Pressione para pegar a direcao</Text>
+            </MapView.Callout>
+           </MapView.Marker>
 
-          const place = (scrolled > 0) ? Math.ceil(scrolled / Dimensions.get('window').width) : 0;
-          console.log('width',  Dimensions.get('window').width);
-          console.log(scrolled);
-          console.log(place);
-          const {latitude, longitude, mark} = places[place];
-          mapView.animateToCoordinate({
-            latitude,
-            longitude,
-          }, 1000);
+           <MapView.Marker coordinate={origin}>
+             <MapView.Callout>
 
-          setTimeout(() => {
-            mark.showCallout();
-          }, 1000);
-        }}
-        >
-        {places.map(place => (
-          <View style={styles.place} key={place.id}>
-            <Text>{place.title}</Text>
-            <Text>{place.description}</Text>
-          </View>
-        ))}
-      </ScrollView>
+             <Text>Voce está aqui</Text>
+             </MapView.Callout>
+           </MapView.Marker>
+
+           <MapViewDirections
+           origin={origin}
+           destination={destination}
+           apikey={MAPS_API_KEY}
+           strokeWidth={5}
+           strokeColor="black" />
+         </MapView>
+
+         <View style={styles.inputContainer}>
+            <TextInput
+            style={styles.input}
+            onChangeText={(text) => {setOriginText(text)}}
+            place="Origem"
+            value={originText} />
+
+            <TextInput
+            style={styles.input}
+            onChangeText={(text) => {setDestinationText(text)}}
+            place="Destino"
+
+            value={destinationText} />
+
+          <TouchableOpacity style={styles.button} onPress={handleButton}>
+            <Text style={styles.buttonText}>Calcular rota</Text>
+          </TouchableOpacity>
+         </View>
+
     </View>
   );
 };
@@ -155,18 +221,23 @@ const styles = StyleSheet.create({
     alignItems: 'flex-end',
     backgroundColor: 'red',
   },
-  placesContainer: {
+  inputContainer: {
     width: '100%',
     maxHeight: 200,
   },
-  place: {
+  buttonText: {
+
+    color: '#000',
+    fontWeight: 'bold',
+
+  },
+
+  input: {
     width: width - 40,
     maxHeight: 200,
     backgroundColor: '#FFF',
+    marginBottom: 15,
     marginHorizontal: 20,
-    borderTopLeftRadius: 10,
-    borderTopRightRadius: 10,
-    padding: 20,
   },
   MapView: {
     position: 'absolute',
@@ -174,6 +245,20 @@ const styles = StyleSheet.create({
     left: 0,
     bottom: 0,
     right: 0,
+  },
+  button: {
+
+    width: width - 100,
+    height: 40,
+    backgroundColor: '#FFF',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderWidth: 1,
+    borderColor: '#000',
+    borderRadius: 7,
+    marginBottom: 15,
+    marginHorizontal: 20,
+
   },
 });
 
